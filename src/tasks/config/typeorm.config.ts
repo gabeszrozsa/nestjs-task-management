fa64230092360f1d2628425aba511a18d1d5066b
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2019-08-03 14:22:07 +0200

    TypeORM

---
+++ src/tasks/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.ts'], // any folder, any file with entity
+  synchronize: true,
+};
