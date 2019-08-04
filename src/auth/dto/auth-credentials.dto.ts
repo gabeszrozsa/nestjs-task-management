e137ea0f7e2cf29693ab811413aca6465fc97494
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2019-08-04 23:08:32 +0200

    AuthCredentialsDto validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  // at least one uppercase + lowercase + number/spec char
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
   password: string;
 }
